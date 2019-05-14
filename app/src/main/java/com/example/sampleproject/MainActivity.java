2c6a39fe5a8e86ef783780b30127d6d36a572a49
wisdompark96 <youo78@naver.com>
2019-05-14 19:03:34 +0900

    fragment 수정

--- app/src/main/java/com/example/sampleproject/MainActivity.java
+++ app/src/main/java/com/example/sampleproject/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.sampleproject.databinding.ActivityMainBinding;
+import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +20,35 @@ public class MainActivity extends AppCompatActivity {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.setActivity(this);
+        setSupportActionBar(binding.toolbar);
+
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Month"));
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Week"));
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Day"));
+        binding.tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        TabPagerAdapter tabPagerAdapter = new TabPagerAdapter(getSupportFragmentManager(), binding.tabLayout.getTabCount());
+        binding.pager.setAdapter(tabPagerAdapter);
+
+        binding.pager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(binding.tabLayout));
+
+        binding.tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                binding.pager.setCurrentItem(tab.getPosition());
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
 
     }
 
--- app/src/main/java/com/example/sampleproject/MonthFragment.java
+++ app/src/main/java/com/example/sampleproject/MonthFragment.java
@@ -10,13 +10,26 @@ import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
+import com.example.sampleproject.databinding.FragmentMonthlyBinding;
+
 public class MonthFragment extends Fragment {
+
+    FragmentMonthlyBinding binding;
+
+    public static MonthFragment newInstance(){
+        Bundle args = new Bundle();
+
+        MonthFragment monthFragment = new MonthFragment();
+        monthFragment.setArguments(args);
+        return monthFragment;
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_monthly,container, false);
 
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return binding.getRoot();
     }
 
     @Override
